c2ae42bba3ae5f0740fe72d89be630b91c860a89
Crisel <justus.walter-helk@web.de>
2025-12-08 12:57:05 +0100

    log source file on warn/error/critical

--- Basen/Source/Core/Logging.h
+++ Basen/Source/Core/Logging.h
@@ -16,15 +16,29 @@ namespace Basen {
 		static std::shared_ptr<spdlog::logger> s_ApplicationLogger;
 	};
 
+// Engine logging
 #define BAS_EN_TRACE(...) ::Basen::Logging::getEngineLogger()->trace(__VA_ARGS__)
-#define BAS_EN_INFO(...) ::Basen::Logging::getEngineLogger()->info(__VA_ARGS__)
-#define BAS_EN_WARN(...) ::Basen::Logging::getEngineLogger()->warn(__VA_ARGS__)
-#define BAS_EN_ERROR(...) ::Basen::Logging::getEngineLogger()->error(__VA_ARGS__)
-#define BAS_EN_CRITICAL(...) ::Basen::Logging::getEngineLogger()->critical(__VA_ARGS__)
+#define BAS_EN_INFO(...)  ::Basen::Logging::getEngineLogger()->info(__VA_ARGS__)
 
+#define BAS_EN_WARN(...) ::Basen::Logging::getEngineLogger()->log( \
+    spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::warn, __VA_ARGS__)
+
+#define BAS_EN_ERROR(...) ::Basen::Logging::getEngineLogger()->log( \
+    spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::err, __VA_ARGS__)
+
+#define BAS_EN_CRITICAL(...) ::Basen::Logging::getEngineLogger()->log( \
+    spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::critical, __VA_ARGS__)
+
+// Application logging
 #define BAS_APP_TRACE(...) ::Basen::Logging::getApplicationLogger()->trace(__VA_ARGS__)
-#define BAS_APP_INFO(...) ::Basen::Logging::getApplicationLogger()->info(__VA_ARGS__)
-#define BAS_APP_WARN(...) ::Basen::Logging::getApplicationLogger()->warn(__VA_ARGS__)
-#define BAS_APP_ERROR(...) ::Basen::Logging::getApplicationLogger()->error(__VA_ARGS__)
-#define BAS_APP_CRITICAL(...) ::Basen::Logging::getApplicationLogger()->critical(__VA_ARGS__)
+#define BAS_APP_INFO(...)  ::Basen::Logging::getApplicationLogger()->info(__VA_ARGS__)
+
+#define BAS_APP_WARN(...) ::Basen::Logging::getApplicationLogger()->log( \
+    spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::warn, __VA_ARGS__)
+
+#define BAS_APP_ERROR(...) ::Basen::Logging::getApplicationLogger()->log( \
+    spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::err, __VA_ARGS__)
+
+#define BAS_APP_CRITICAL(...) ::Basen::Logging::getApplicationLogger()->log( \
+    spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, spdlog::level::critical, __VA_ARGS__)
 }
